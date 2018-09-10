b0950121e22afb266999c7f0e1966caea99dd317
Parharr4 <35544325+Parharr4@users.noreply.github.com>
2018-09-10 16:30:20 -0400

    Update math.c
    Comments

--- Assignment/math.c
+++ Assignment/math.c
@@ -37,19 +37,34 @@ int operations(int num1, int num2, char Operator, int result)
             break;
        default: printf("Not an Operator please use: +, -, *, /, %', <, >, &, |, ^, ~, `");
             break;
+      /*
+      ** + Add 
+      * - Subtract 
+      * * Multiply 
+      * / Divide 
+      * % Modulus 
+      * < Left Shift 
+      * > Right Shift 
+      * & Bitwise AND 
+      * | Bitwise OR 
+      * ^ Bitwise XOR 
+      * ~ Bitwise Inverse Num1
+      * ` Bitwise Inverse Num2
+      */
 }
 }
 int main()
 {
-    int num1 = 5, num2 = 3;
+    int num1 = 5, num2 = 3; // Variables to change
     char Operator = '|';
-    int result = 0;
+    int result = 0; //Don't change this number automatically changes
    // printf("Choose an Operator: +, -, *, /, %', <, >, &, |, ^, ~ used for num1 ,` used for num2 \n");
    // scanf("%c", Operator);
    // printf("Input first integer value \n");
    // scanf("%i", num1);
    // printf("Input Second integer value \n");
    // scanf("%i" , num2);    
+   // Attempted User input
     result = operations(num1, num2 , Operator, result);
     printf("%i %c %i = %i \n", num1, Operator, num2, result);
     
